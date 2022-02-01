fae2e5c42fd9e6a72b5f5e28fa14764a6c48b5bc
caio-chcn <caioh.mec@gmail.com>
2022-02-01 13:41:05 -0300

    sexto desafio

---
+++ script.js
@@ -0,0 +1,50 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+let eu ={
+    nome:"Caio",
+    idade:30,
+    cor:"negro"
+}
+console.log(eu)
+
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+eu.altura = "1,87"
+console.log(eu)
+
+// Remova uma propriedade desse objeto.
+eu.idade
+console.log(eu)
+
+//Mostre no console todas as propriedades desse objeto.
+
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+let cadastro = [
+    {nome:"leticia",
+     idade:"20",
+     telefone:"31 1234 5678",
+     amigos:"ruana","igor","marcela","greisson",
+    },
+    {nome:"ruana",
+     idade:"28",
+     telefone:"31 2345 6789",
+     amigos:"leticia","igor","marcela","greisson",
+    },
+    {nome:"igor",
+    idade:"26",
+    telefone:"31 3456 7890",
+    amigos:"leticia","ruana","marcela",greisson",
+    },
+    {nome:"marcela",
+    idade:"34",
+    telefone:"31 1357 9357",
+    amigos:"leticia","ruana","igor","greisson",
+    },
+    {nome:"greisson",
+    idade:"32",
+    telefone:"21 2468 0246",
+    amigos:"leticia","ruana","igor","marcela",
+    },
+    ]
+    
